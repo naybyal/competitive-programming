e8f3f71f235d51e688f4cb89cfe6c13189db56d9
naybyal <officiallynabiel@gmail.com>
2024-06-16 12:20:41 +0530

    input/output

---
+++ cpp-basics/io.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World" << endl;  
+  int a, b;
+  string x;
+
+  cin >> a >> b >> x;
+  
+  cout << a << b << x;
+}
